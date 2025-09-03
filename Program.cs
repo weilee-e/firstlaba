182c761f5c0c865f3a9c6da1c83dcd8ff9c3e1d4
Mimimi <SAPR@PC-NXT>
2025-09-03 14:36:19 +0700

    nachalo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System;
+class Calculator
+{
+    static void Main()
+    {
+        double num1, num2, num3;
+        string operation;
+
+        Console.WriteLine("Введите num1");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите операцию");
+        operation = Console.ReadLine();
+
+        Console.WriteLine("Введите num2");
+        num2 = Convert.ToDouble(Console.ReadLine());
+}
+}
+    
+
